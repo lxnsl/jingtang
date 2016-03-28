5cf36c63e62ba04847cb4e74e4d8d06ec68b26f9
zouxin <zouxin@zouxindemacintosh.local>
2016-03-28 11:28:09 +0800

    Initial Commit

---
+++ 金堂/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  金堂
+//
+//  Created by zouxin on 16/3/28.
+//  Copyright © 2016年 luxia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 金堂/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  金堂
+//
+//  Created by zouxin on 16/3/28.
+//  Copyright © 2016年 luxia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
