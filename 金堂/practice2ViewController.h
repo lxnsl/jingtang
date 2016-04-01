22e46f9b1f011d1762d1a877f60f570bd8db8c69
SDKJtechnology <zouxin@zouxindemacintosh.local>
2016-04-01 19:13:34 +0800

    practice2

---
+++ 金堂/practice2ViewController.h
@@ -0,0 +1,13 @@
+//
+//  practice2ViewController.h
+//  金堂
+//
+//  Created by zouxin on 16/4/1.
+//  Copyright © 2016年 luxia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface practice2ViewController : UIViewController
+
+@end
