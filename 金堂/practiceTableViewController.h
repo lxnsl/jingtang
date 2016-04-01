922c7ab72ef7259bce5351924461f6a75b6ad1b5
SDKJtechnology <zouxin@zouxindemacintosh.local>
2016-04-01 18:04:18 +0800

    1432434rrewre

---
+++ 金堂/practiceTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  practiceTableViewController.h
+//  金堂
+//
+//  Created by zouxin on 16/4/1.
+//  Copyright © 2016年 luxia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface practiceTableViewController : UITableViewController
+
+@end
